27df0c7c379132c4147484d405ffdd205496d875
aliyu bin ahmad <aliyubinahmad2022@gmail.com>
2026-01-02 09:57:30 +0100

    moving routing down after settings load

--- ksitm_vlcp/asgi.py
+++ ksitm_vlcp/asgi.py
@@ -1,16 +1,19 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import main.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ksitm_vlcp.settings")
 
 django_asgi_app = get_asgi_application()
 
+import main.routing  # 👈 IMPORT AFTER SETTINGS ARE READY
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(main.routing.websocket_urlpatterns)
+        URLRouter(
+            main.routing.websocket_urlpatterns
+        )
     ),
 })
